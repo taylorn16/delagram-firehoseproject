dfab9e7b879aac202e810c78461ef8002bc35ac5
Nick Taylor <taylorn@udel.edu>
2016-03-21 22:53:31 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Delagram::Application.config.secret_key_base = '03c109ed06f9bd3dec9b8568626fb6b4a5e4ab96ba21ff4c98e311ab001619917999581a4a882d43f7643137cf9564d6b73549d38184cee18dc99d80eba6054b'
