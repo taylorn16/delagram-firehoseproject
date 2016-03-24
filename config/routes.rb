aac966bb41d501edf062a478b319090e37ebff91
Nick Taylor <taylorn@udel.edu>
2016-03-24 21:01:29 +0000

    Added test for successful homepage request

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Delagram::Application.routes.draw do
+
+  root to: 'grams#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
